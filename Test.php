a1ff19bbed771a7c60f162e78e5acf4cff342264
ketchan19 <ketchan19@users.noreply.github.com>
2018-05-17 17:27:42 +0800

    Update Test.php

--- Test.php
+++ Test.php
@@ -4,27 +4,6 @@ class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
 		$this->assertEquals(1+2,1);
-  	}1awd
+  	}
 }
-asdasd
-	sadsa
-	d
-	sa
-	das
-	d
-	asd
-	as
-	dsa
-	d
-	as
-	das
-	d
-	as
-	das
-	d
-	sad
-	as
-	d
-	sa
-	d
 ?>
